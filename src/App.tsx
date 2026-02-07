9d193d0c6c0cf988b1ddc44cd684bf238a522081
Denis <Ori21wiki@gmail.com>
2026-02-07 14:47:48 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      <div className='w-40 h-40 bg-blue-500 rounded-lg'>Центр</div>
+    </div>
+  );
+}
+
+export default App;
