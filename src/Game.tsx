011ac2baab10d2d417b8c25d122a5d628f3869b8
Denis <Ori21wiki@gmail.com>
2026-02-07 15:31:06 +0300

    base_logic

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
+
 export const Game = () => {
+  const [cells, setCells] = useState(Array(9).fill(null));
+  const [isX, setIsX] = useState(true);
+  console.log(cells);
+
+  const handleClick = (i: number) => {
+    if (cells[i]) {
+      return;
+    }
+    const newCells = [...cells];
+    newCells[i] = isX ? '❌' : '⭕';
+    setCells(newCells);
+    setIsX(!isX);
+  };
+
   return (
-    <div className='w-90 h-90 bg-blue-500 border grid grid-cols-3 text-black font-bold '>
-      <div className='flex items-center justify-center border '>1</div>
-      <div className='flex items-center justify-center border '>2</div>
-      <div className='flex items-center justify-center border '>3</div>
-      <div className='flex items-center justify-center border '>4</div>
-      <div className='flex items-center justify-center border '>5</div>
-      <div className='flex items-center justify-center border '>6</div>
-      <div className='flex items-center justify-center border '>7</div>
-      <div className='flex items-center justify-center border '>8</div>
-      <div className='flex items-center justify-center border '>9</div>
+    <div className='w-90 h-90 border grid grid-cols-3 text-black font-bold '>
+      {cells.map((value, i) => (
+        <button
+          key={i}
+          onClick={() => handleClick(i)}
+          className='flex items-center justify-center border w-30 h-30'
+        >
+          {value}
+        </button>
+      ))}
     </div>
   );
 };
